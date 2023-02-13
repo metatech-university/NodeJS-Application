24cf9b32e28d51b43abd5102d2afbd88613cb01a
Dmytro Nechai <nechaido@gmail.com>
2023-02-13 10:30:58 +0200

    Comply with metarhia style application

--- config/server.js
+++ config/server.js
@@ -4,6 +4,28 @@
   protocol: 'http',
   ports: [8001],
   nagle: false,
+  timeouts: {
+    bind: 2000,
+    start: 30000,
+    stop: 5000,
+    request: 5000,
+    watch: 1000,
+  },
+  queue: {
+    concurrency: 1000,
+    size: 2000,
+    timeout: 3000,
+  },
+  scheduler: {
+    concurrency: 10,
+    size: 2000,
+    timeout: 3000,
+  },
+  workers: {
+    pool: 2,
+    wait: 2000,
+    timeout: 5000,
+  },
   cors: {
     origin: '*',
   },
--- config/sessions.js
+++ config/sessions.js
@@ -6,4 +6,8 @@
   regenerate: 60 * 60 * 1000,
   expire: 2 * 60 * 60 * 1000,
   persistent: true,
+  limits: {
+    ip: 20,
+    user: 5,
+  },
 });
