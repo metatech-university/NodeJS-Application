9d9b5e815ae6c38e79ed3a76e48ef7c33b1634b7
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-02-16 12:52:11 +0200

    fixup! Refactor api structure for new loader

--- api/auth/signin.js
+++ api/auth/signin.js
@@ -1,4 +1,7 @@
-async ({ login, password }) => {
-  console.log({ method: 'auth.signin', login, password });
-  return { status: 'ok', token: '--no-token-provided--' };
-};
+({
+  access: 'public',
+  method: async ({ login, password }) => {
+    console.log({ method: 'auth.signin', login, password });
+    return { status: 'ok', token: '--no-token-provided--' };
+  },
+});
