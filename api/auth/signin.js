12b84af0349cf9365f638625c3f6b75c6e5975da
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-03-09 12:33:32 +0200

    Add initial `auth` module implementation

--- api/auth/signin.js
+++ api/auth/signin.js
@@ -1,7 +1,17 @@
 ({
   access: 'public',
   method: async ({ login, password }) => {
-    console.log({ method: 'auth.signin', login, password });
-    return { status: 'ok', token: '--no-token-provided--' };
+    const user = await api.auth.provider.getUser(login);
+    if (!user) throw new Error('Incorrect login or password');
+    const { accountId, password: hash } = user;
+    const valid = await metarhia.metautil.validatePassword(password, hash);
+    if (!valid) throw new Error('Incorrect login or password');
+    console.log(`Logged user: ${login}`);
+    const token = api.auth.provider.generateToken();
+    const data = { accountId: user.accountId };
+    context.client.startSession(token, data);
+    const { ip } = context.client;
+    api.auth.provider.startSession(token, data, { ip, accountId });
+    return { status: 'logged', token };
   },
 });
