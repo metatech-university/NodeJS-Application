5eb76a2ead21acac348f061565aced7dc33e7cec
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-05-08 23:52:56 +0300

    Update eslint config and fix code style

--- domain/Chat/repository/get.js
+++ domain/Chat/repository/get.js
@@ -1,5 +1,5 @@
 async (ctx, name) => {
-  const sql = `SELECT * FROM "Chat" WHERE "name" = $1`;
+  const sql = 'SELECT * FROM "Chat" WHERE "name" = $1';
   const { rows: [chat] } = ctx.state.pg.query(sql, [name]);
   return chat;
 };
