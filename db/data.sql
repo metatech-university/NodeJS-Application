a1eb8a023a54fba43a9e273f6cbf144c9960fd02
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-05-27 23:26:15 +0300

    Fix ids in initial data

--- db/data.sql
+++ db/data.sql
@@ -1,8 +1,18 @@
-INSERT INTO "Account" ("login", "password") VALUES
-  ('admin', 'ypMEd9FwvtlOjcvH94iICQ==:V6LnSOVwXzENxeLCJk59Toadea7oaA1IxYulAOtKkL9tBxjEPOw085vYalEdLDoe8xbrXQlhh7QRGzrSe8Bthw=='),
-  ('marcus', 'dpHw0OUNBz76nuqrXZbeYQ==:wpvUVgi8Yp9rJ0yZyBWecaWP2EL/ahpxZY74KOVfhAYbAZSq6mWqjsQwtCvIPcSKZqUVpVb13JcSciB2fA+6Tg=='),
-  ('user', 'r8zb8AdrlPSh5wNy6hqOxg==:HyO5rvOFLtwzU+OZ9qFi3ADXlVccDJWGSfUS8mVq43spJ6sxyliUdW3i53hOPdkFAtDn3EAQMttOlIoJap1lTQ=='),
-  ('iskandar', 'aqX1O4bKXiwC/Jh2EKNIYw==:bpE4TARNg09vb2Libn1c00YRxcvoklB9zVSbD733LwQQFUuAm7WHP85PbZXwEbbeOVPIFHgflR4cvEmvYkr76g==');
+INSERT INTO "Identifier" DEFAULT VALUES;
+INSERT INTO "Account" ("id", "login", "password")
+VALUES (lastval(), 'admin', 'ypMEd9FwvtlOjcvH94iICQ==:V6LnSOVwXzENxeLCJk59Toadea7oaA1IxYulAOtKkL9tBxjEPOw085vYalEdLDoe8xbrXQlhh7QRGzrSe8Bthw==');
+
+INSERT INTO "Identifier" DEFAULT VALUES;
+INSERT INTO "Account" ("id", "login", "password")
+VALUES (lastval(), 'marcus', 'dpHw0OUNBz76nuqrXZbeYQ==:wpvUVgi8Yp9rJ0yZyBWecaWP2EL/ahpxZY74KOVfhAYbAZSq6mWqjsQwtCvIPcSKZqUVpVb13JcSciB2fA+6Tg==');
+
+INSERT INTO "Identifier" DEFAULT VALUES;
+INSERT INTO "Account" ("id", "login", "password")
+VALUES (lastval(), 'user', 'r8zb8AdrlPSh5wNy6hqOxg==:HyO5rvOFLtwzU+OZ9qFi3ADXlVccDJWGSfUS8mVq43spJ6sxyliUdW3i53hOPdkFAtDn3EAQMttOlIoJap1lTQ==');
+
+INSERT INTO "Identifier" DEFAULT VALUES;
+INSERT INTO "Account" ("id", "login", "password")
+VALUES (lastval(), 'iskandar', 'aqX1O4bKXiwC/Jh2EKNIYw==:bpE4TARNg09vb2Libn1c00YRxcvoklB9zVSbD733LwQQFUuAm7WHP85PbZXwEbbeOVPIFHgflR4cvEmvYkr76g==');
 
 -- Examples login/password
 -- admin/123456
@@ -11,15 +21,15 @@ INSERT INTO "Account" ("login", "password") VALUES
 -- iskandar/zulqarnayn
 
 INSERT INTO "Area" ("name", "ownerId") VALUES
-  ('Metarhia', 2),
-  ('Metaeducation', 2);
+  ('Metarhia', (SELECT "id" FROM "Account" WHERE "login" = 'marcus')),
+  ('Metaeducation', (SELECT "id" FROM "Account" WHERE "login" = 'marcus'));
 
 INSERT INTO "AreaAccount" ("areaId", "accountId") VALUES
-  (1, 1),
-  (1, 2),
-  (1, 3),
-  (1, 4),
-  (2, 1),
-  (2, 2),
-  (2, 3),
-  (2, 4);
+  ((SELECT "areaId" FROM "Area" WHERE "name" = 'Metarhia'), (SELECT "id" FROM "Account" WHERE "login" = 'admin')),
+  ((SELECT "areaId" FROM "Area" WHERE "name" = 'Metarhia'), (SELECT "id" FROM "Account" WHERE "login" = 'marcus')),
+  ((SELECT "areaId" FROM "Area" WHERE "name" = 'Metarhia'), (SELECT "id" FROM "Account" WHERE "login" = 'user')),
+  ((SELECT "areaId" FROM "Area" WHERE "name" = 'Metarhia'), (SELECT "id" FROM "Account" WHERE "login" = 'iskandar')),
+  ((SELECT "areaId" FROM "Area" WHERE "name" = 'Metaeducation'), (SELECT "id" FROM "Account" WHERE "login" = 'admin')),
+  ((SELECT "areaId" FROM "Area" WHERE "name" = 'Metaeducation'), (SELECT "id" FROM "Account" WHERE "login" = 'marcus')),
+  ((SELECT "areaId" FROM "Area" WHERE "name" = 'Metaeducation'), (SELECT "id" FROM "Account" WHERE "login" = 'user')),
+  ((SELECT "areaId" FROM "Area" WHERE "name" = 'Metaeducation'), (SELECT "id" FROM "Account" WHERE "login" = 'iskandar'));
