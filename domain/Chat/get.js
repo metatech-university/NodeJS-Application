3c48f8cdbd6fd7e3887d94f30288f55e87a2fd49
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-05-28 00:32:02 +0300

    Update lib and domain examples

--- domain/Chat/get.js
+++
@@ -1,7 +0,0 @@
-async (ctx, name) => {
-  const { repository } = domain.Chat;
-
-  const chat = await repository.get(ctx, name);
-  if (!chat) throw new domain.Errors.NotFoundError('No such chat', { name });
-  return chat;
-};
--- domain/Chat/repository/get.js
+++
@@ -1,5 +0,0 @@
-async (ctx, name) => {
-  const sql = 'SELECT * FROM "Chat" WHERE "name" = $1';
-  const { rows: [chat] } = ctx.state.pg.query(sql, [name]);
-  return chat;
-};
