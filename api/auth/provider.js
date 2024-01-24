c99957412fc5262bb3a93d9282e587a65253be71
Alex Buglak <alexeybuglak24@gmail.com>
2024-01-24 15:53:07 +0100

    fixed signin and provider

--- api/auth/provider.js
+++ api/auth/provider.js
@@ -1,33 +1,35 @@
 ({
   generateToken() {
     const { characters, secret, length } = config.sessions;
-    return metarhia.metautil.generateToken(secret, characters, length);
+    return common.generateToken(secret, characters, length);
   },
 
   saveSession(token, data) {
-    db.pg.update('Session', { data: JSON.stringify(data) }, { token });
+    return db('Session').update(token, { data: JSON.stringify(data) });
   },
 
   startSession(token, data, fields = {}) {
     const record = { token, data: JSON.stringify(data), ...fields };
-    db.crud('Session').create(record);
+    return db('Session').create(record);
   },
 
   async restoreSession(token) {
-    const record = await db.pg.row('Session', ['data'], { token });
+    const record = await db('Session').read(token, 'token', ['data']);
     if (record && record.data) return record.data;
     return null;
   },
 
   deleteSession(token) {
-    db.pg.delete('Session', { token });
+    return db('Session').delete(token, 'token');
   },
 
   async registerUser(login, password) {
-    return db.pg.insert('Account', { login, password });
+    return db('Account').create({ login, password });
   },
 
   async getUser(login) {
-    return db.crud('Account').read(login, 'login');
+    return db('Account')
+      .read(login, 'login')
+      .then((u) => u.rows[0]);
   },
 });
--- api/auth/signin.js
+++ api/auth/signin.js
@@ -1,18 +1,15 @@
 ({
   access: 'public',
   method: async ({ login, password }) => {
-    const user = await api.auth
-      .provider()
-      .getUser(login)
-      .then((u) => u.rows[0]);
+    const user = await api.auth.provider().getUser(login);
     if (!user) throw new Error('Incorrect login or password');
     const { id: accountId, password: hash } = user;
-    const valid = await metarhia.metautil.validatePassword(password, hash);
+    const valid = await common.validatePassword(password, hash);
     if (!valid) throw new Error('Incorrect login or password');
     console.log(`Logged user: ${login}`);
     const token = api.auth.provider().generateToken();
     const data = { accountId: user.id };
-    context.client.startSession(token, data); // dont exist jet
+    // context.client.startSession(token, data); // TODO: doesn't exist yet
     const { ip } = context.client;
     api.auth.provider().startSession(token, data, { ip, accountId });
     return { status: 'logged', token };
